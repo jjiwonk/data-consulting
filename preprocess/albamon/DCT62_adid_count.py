178ce01a9a39aa4e91532e72a7742efb30f4f657
jhha0122 <jhha@madup.com>
2022-08-12 10:20:48 +0900

    fix

--- preprocess/albamon/DCT62_adid_count.py
+++ preprocess/albamon/DCT62_adid_count.py
@@ -1,8 +1,9 @@
 import setting.directory as dr
 from preprocess.albamon import raw_data
 import pyarrow as pa
+import pandas as pd
 
-raw_dir = dr.dropbox_dir + ' (주식회사매드업)/광고사업부/4. 광고주/알바몬/4-1. 광고주 제공자료/애드저스트 RAW/7월'
+raw_dir = dr.dropbox_dir + '/광고사업부/4. 광고주/알바몬/4-1. 광고주 제공자료/애드저스트 RAW/7월'
 dtypes = {
     '{activity_kind}': pa.string(),
     '{event_name}': pa.string(),
@@ -11,7 +12,14 @@ dtypes = {
 }
 
 df = raw_data.adjust_data_read(raw_dir = raw_dir, dtypes = dtypes, len_of_folder=9, media_filter=['Organic'])
+df.head()
 
-print(df.head())
+event_names = ["이력서작성_최초등록","이력서작성_추가등록","개인 회원가입","기업 회원가입","무료공고등록","유료공고등록","온라인 지원","간편문자지원","이메일지원","전화지원","홈페이지지원","일반문자지원"]
+check = df[(df["{activity_kind}"] == "event")&(df['{event_name}'].isin(event_names))]
 
-df['{adid}'].unique().count()
+len(check['{adid}'].unique())
+# 조건에 맞는 이벤트 전환이 발생한 ADID 갯수
+
+adid_list = pd.DataFrame(check['{adid}'].unique(), columns=['adid'])
+adid_list.to_csv(dr.download_dir + "/adid_list.csv", encoding='utf-8-sig', index=False)
+# 조건에 맞는 이벤트 전환이 발생한 ADID 리스트
