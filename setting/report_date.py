7e7cfd843853d673adb92acf9230133ddecd8d1c
jsmin <jsmin@madit.kr>
2022-08-19 11:43:17 +0900

    fix(report_date) : 날짜 설정 수정

--- setting/report_date.py
+++ setting/report_date.py
@@ -1,8 +1,8 @@
 import datetime
 
 today = datetime.date.today()
-day_1 = datetime.date(year=today.year, month=today.month, day=1)
-day_1 = day_1 - datetime.timedelta(1)
+#today = datetime.date(year = 2022, month = 8, day = 1)
+day_1 = today - datetime.timedelta(1)
 start_day = datetime.date(year=day_1.year, month=day_1.month, day=1)
 yearmonth = day_1.strftime('%Y%m')
 month_name = str(day_1.month) + '월'
