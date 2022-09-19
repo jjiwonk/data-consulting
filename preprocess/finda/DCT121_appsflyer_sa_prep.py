a724dfaf5d0aaf09bb932c63aecac35209e8f3cc
jhha0122 <jhha@madup.com>
2022-09-19 17:22:30 +0900

    fix(DCT121): finda sa 앱스 코드 리팩토링

--- preprocess/finda/DCT121_appsflyer_sa_prep.py
+++ preprocess/finda/DCT121_appsflyer_sa_prep.py
@@ -1,14 +1,22 @@
-import os
 # !pip install xlsxwriter
+# !pip install pyarrow
+
 import setting.directory as dr
+import setting.report_date as rdate
+
+import os
 import pyarrow as pa
 import pyarrow.csv as pacsv
 import pandas as pd
-import datetime
-from dateutil.relativedelta import relativedelta
 import numpy as np
 
-def get_raw_df(raw_dir, required_date):
+
+raw_dir = dr.dropbox_dir + '/광고사업부/4. 광고주/핀다_7팀/2. 리포트/자동화리포트/appsflyer_prism'
+result_dir = dr.download_dir
+media_source = ['naver_sa_mo_main', '네이버sa', 'naversa', 'kakaosa', 'googlesa', 'naver_sa_mo_direct',
+                'naversamo', 'naversapc', 'googlesamo', 'googlesapc', 'googleadwords_int']
+
+def get_raw_df(raw_dir, required_date, media_source):
     dtypes = {
         'attributed_touch_type': pa.string(),
         'attributed_touch_time': pa.string(),
@@ -52,10 +60,9 @@ def get_raw_df(raw_dir, required_date):
     ro = pacsv.ReadOptions(block_size=10 << 20)
     table_list = []
 
-    given_date = datetime.datetime.strptime(required_date,'%Y-%m-%d')
-    date_check = given_date.strftime('%Y%m')
-    start_date = given_date.replace(day=1).strftime('%Y%m%d')
-    end_date = given_date.strftime('%Y%m%d')
+    date_check = required_date.strftime('%Y%m')
+    start_date = required_date.replace(day=1).strftime('%Y%m%d')
+    end_date = required_date.strftime('%Y%m%d')
 
     files = os.listdir(raw_dir)
     files = [f for f in files if '.csv' in f and str(f)[-12:-6] == date_check]
@@ -68,10 +75,9 @@ def get_raw_df(raw_dir, required_date):
     table = pa.concat_tables(table_list)
     raw_df = table.to_pandas()
     raw_df.media_source = raw_df.media_source.str.lower()
-    raw_df = raw_df.loc[raw_df['media_source'].isin(['naver_sa_mo_main', '네이버sa', 'naversa', 'kakaosa',
-                                                                'googlesa', 'naver_sa_mo_direct', 'naversamo', 'naversapc',
-                                                                'googlesamo', 'googlesapc', 'googleadwords_int'])]
+    raw_df = raw_df.loc[raw_df['media_source'].isin(media_source)]
     return raw_df
+
 
 def prep_data(raw_df):
     prep_df = raw_df
@@ -108,10 +114,17 @@ def prep_data(raw_df):
     prep_df.loc[prep_df['adset'] == '', 'adset'] = '-'
     prep_df.loc[prep_df['ad'] == '', 'ad'] = '-'
 
+    prep_df.loc[prep_df['ad'] == 'utm_content', 'keywords'] = prep_df['adset']
+    prep_df.loc[prep_df['ad'] == 'utm_content', ['adset', 'ad']] = '-'
+    prep_df.loc[(prep_df['ad'] == 'bridge_finda-brand-2_2207')&(prep_df['adset'] == '대출'), 'keywords'] = prep_df['adset']
+    prep_df.loc[(prep_df['ad'] == 'bridge_finda-brand-2_2207') & (prep_df['adset'] == '대출'), ['adset', 'ad']] = '-'
+    # 추가 예외처리
+
     return prep_df
 
+
 def download_df(prep_df, required_date, result_dir):
-    date = datetime.datetime.strptime(required_date, '%Y-%m-%d').strftime('%m%d')
+    date = required_date.strftime('%m%d')
     writer = pd.ExcelWriter(result_dir + f'/종합_{date}.xlsx', engine='xlsxwriter', engine_kwargs={'options':{'strings_to_urls': False}})
 
     # 데이터 구분
@@ -140,11 +153,9 @@ def download_df(prep_df, required_date, result_dir):
     print('download success')
 
 
-raw_dir = dr.dropbox_dir + '/광고사업부/4. 광고주/핀다_7팀/2. 리포트/자동화리포트/appsflyer_prism'
-result_dir = dr.download_dir
-# 전일자 yyyy-mm-dd 형식으로 입력
-required_date = '2022-09-02'
-
-raw_df = get_raw_df(raw_dir, required_date)
+required_date = rdate.day_1
+raw_df = get_raw_df(raw_dir, required_date, media_source)
 prep_df = prep_data(raw_df)
+
+
 download_df(prep_df, required_date, result_dir)
