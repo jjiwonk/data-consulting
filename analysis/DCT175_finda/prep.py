2d47997c6c42c0ec3e4491686780569270a94649
민정수 <jsmin@madup.com>
2022-10-10 14:41:44 +0900

    fix(DCT105) : 핀다 분석 전처리 코드 리팩토링

--- analysis/DCT175_finda/prep.py
+++ analysis/DCT175_finda/prep.py
@@ -6,7 +6,7 @@ import pyarrow.csv as pacsv
 import pandas as pd
 import numpy as np
 
-def get_paid_df(paid_dir):
+def get_paid_df():
     dtypes = {
         'attributed_touch_time': pa.string(),
         'attributed_touch_type': pa.string(),
@@ -14,6 +14,8 @@ def get_paid_df(paid_dir):
         'event_time': pa.string(),
         'event_name': pa.string(),
         'media_source': pa.string(),
+        'adset' : pa.string(),
+        'ad' : pa.string(),
         'campaign': pa.string(),
         'appsflyer_id': pa.string(),
         'is_retargeting': pa.string(),
@@ -26,20 +28,32 @@ def get_paid_df(paid_dir):
     ro = pacsv.ReadOptions(block_size=10 << 20)
     table_list = []
 
-    files = os.listdir(paid_dir)
+    files = os.listdir(info.paid_dir)
     files = [f for f in files if '.csv' in f]
 
     for f in files:
-        temp = pacsv.read_csv(paid_dir + '/' + f, convert_options=convert_ops, read_options=ro)
+        temp = pacsv.read_csv(info.paid_dir + '/' + f, convert_options=convert_ops, read_options=ro)
         table_list.append(temp)
     table = pa.concat_tables(table_list)
     raw_df = table.to_pandas()
     raw_df['is_organic'] = False
     raw_df.loc[raw_df['attributed_touch_type']!='click', 'is_organic'] = True
     raw_df.loc[(raw_df['platform']=='ios')&(raw_df['attributed_touch_time']==''), 'is_organic'] = False
-    return raw_df
 
-def get_organic_df(organic_dir):
+    # 모비데이즈 데이터 예외처리
+    raw_df_copy = raw_df.copy()
+    raw_df_copy = raw_df_copy.loc[raw_df_copy['media_source']!='']
+    raw_df_copy_dedup = raw_df_copy[['media_source', 'campaign', 'adset', 'ad']].drop_duplicates(['campaign', 'adset', 'ad'])
+    raw_df_copy_dedup = raw_df_copy_dedup.rename(columns = {'media_source' : 'media_source_find'})
+
+    raw_df_merge = raw_df.merge(raw_df_copy_dedup, on = ['campaign', 'adset', 'ad'], how = 'left')
+
+    raw_df_merge.loc[raw_df_merge['media_source']=='', 'media_source'] = raw_df_merge['media_source_find']
+
+    del raw_df_merge['media_source_find']
+    return raw_df_merge
+
+def get_organic_df():
     dtypes = {
         'Attributed Touch Time': pa.string(),
         'Attributed Touch Type': pa.string(),
@@ -58,11 +72,11 @@ def get_organic_df(organic_dir):
     ro = pacsv.ReadOptions(block_size=10 << 20)
     table_list = []
 
-    files = os.listdir(organic_dir)
+    files = os.listdir(info.organic_dir)
     files = [f for f in files if '.csv' in f]
 
     for f in files:
-        temp = pacsv.read_csv(organic_dir + '/' + f, convert_options=convert_ops, read_options=ro)
+        temp = pacsv.read_csv(info.organic_dir + '/' + f, convert_options=convert_ops, read_options=ro)
         table_list.append(temp)
     table = pa.concat_tables(table_list)
     raw_df = table.to_pandas()
@@ -81,8 +95,8 @@ def get_organic_df(organic_dir):
     return raw_df
 
 def raw_data_concat(media_filter = [], from_date='', to_date=''):
-    paid_df = get_paid_df(info.paid_dir)
-    organic_df = get_organic_df(info.organic_dir)
+    paid_df = get_paid_df()
+    organic_df = get_organic_df()
 
     raw_df = pd.concat([paid_df, organic_df], sort = False, ignore_index=True)
 
@@ -128,7 +142,8 @@ def campaign_name_exception(raw_df):
     campaign_mapping_dic = {
         'kakao_int':{
             'madit_ka-friend_loan_br_mo_reach_total':'Madit_KA-FRIEND_LOAN_BR_MO_REACH_TOTAL',
-            'MobidaysAgency_KA-BIZ_LOAN_NU_AEO-CONT_220307':'Madit_KA-BIZ_LOAN_NU_iOS_AEO-CONT_220307'
+            'MobidaysAgency_KA-BIZ_LOAN_NU_AEO-CONT_220307':'Madit_KA-BIZ_LOAN_NU_iOS_AEO-CONT_220307',
+            'Madit_KA-BIZ_LOAN_NU_AEO-CONT_220307' : 'Madit_KA-BIZ_LOAN_NU_iOS_AEO-CONT_220307'
         },
         'KA-FRIEND':{
             'madit_ka-friend_loan_br_mo_reach_total':'Madit_KA-FRIEND_LOAN_BR_MO_REACH_TOTAL'
@@ -136,6 +151,7 @@ def campaign_name_exception(raw_df):
         'moloco_int':{
             'alwayson_loan':'Mobi_Finda_AOS_Viewedlahome',
             'Madit_ML_MYDATA_RT_AOS_AEO-MD_220927_미사용':'Madit_ML_MYDATA_RT_AOS_AEO-MD_220927',
+            'MobidaysAgency_ML_LOAN_NU_iOS_MAIA_220418' : 'Madit_ML_LOAN_NU_iOS_MAIA_220418'
         },
         'cauly_int':{
             'Madit_CAULY_LOAN_NU_AOS_REWARD-CPE':'Madit_CAULY_LOAN_NU_AOS_NCPI_220622'
@@ -158,8 +174,8 @@ def campaign_name_exception(raw_df):
 
     return raw_df
 
-def get_campaign_cost_df(raw_dir, from_date = '', to_date = ''):
-    cost_file = raw_dir + '/핀다_220601_220930 예산.xlsx'
+def get_campaign_cost_df(from_date = '', to_date = ''):
+    cost_file = info.raw_dir + '/핀다_220601_220930 예산.xlsx'
     df_sheet_all = pd.read_excel(cost_file, sheet_name=None, engine='openpyxl')
     df_concat = pd.DataFrame()
     for sheet in df_sheet_all.keys():
@@ -210,7 +226,6 @@ def get_campaign_cost_df(raw_dir, from_date = '', to_date = ''):
     }
     df_concat = df_concat.loc[~(df_concat['매체'].isin(['Facebook', 'Facebook_RE']))]
     df_concat['매체'] = df_concat['매체'].apply(lambda x: media_mapping_dic[x])
-    # df_concat['캠페인'] = df_concat['캠페인'].apply(lambda x: x.lower())
     df_concat = df_concat.reset_index(drop=True)
     df_concat['날짜'] = pd.to_datetime(df_concat['날짜']).dt.date
 
@@ -224,4 +239,32 @@ def get_campaign_cost_df(raw_dir, from_date = '', to_date = ''):
     df_concat = df_concat.rename(columns = {'매체' : 'media_source',
                                             '캠페인' : 'campaign',
                                             '광고비(마크업포함)':'cost'})
+
+    df_concat.loc[df_concat['media_source'] == 'moloco_int', 'campaign'] = df_concat['campaign'].apply(lambda x: str(x).replace('_미사용', ''))
     return df_concat
+
+def get_campaign_cost_hour_df(from_date = '', to_date = ''):
+    cost_file = info.raw_dir + '/(핀다) 일별-시간대별 캠페인 비용_total_221007.csv'
+    hour_df = pd.read_csv(cost_file)
+
+    # 비용 데이터 캠페인 명 > 앱스 캠페인 명 매핑 작업 시
+    media_mapping_dic = {
+        'Google AC': 'googleadwords_int',
+        'Google ACe': 'googleadwords_int',
+        'Kakao': 'kakao_int'
+    }
+
+    hour_df['media_source'] = hour_df['media_source'].apply(lambda x: media_mapping_dic[x])
+
+    hour_df.loc[hour_df['media_source']=='googleadwords_int', 'cost'] = hour_df['cost'] * 1.013
+    hour_df.loc[hour_df['media_source']=='kakao_int', 'cost'] = hour_df['cost'] / 1.1
+
+    hour_df['날짜'] = pd.to_datetime(hour_df['날짜']).dt.date
+
+    if from_date == '':
+        from_date = np.min(hour_df['날짜'])
+    if to_date == '' :
+        to_date = np.max(hour_df['날짜'])
+    hour_df = hour_df.loc[
+        (hour_df['날짜'] >= from_date) & (hour_df['날짜'] <= to_date)]
+    return hour_df
