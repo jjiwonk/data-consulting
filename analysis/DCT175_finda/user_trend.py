ce288d06b5c08607a5cee42f87f70ad0fd80879f
민정수 <jsmin@madup.com>
2022-10-10 16:33:34 +0900

    fix(DCT105) : 유저 트렌드 데이터 리팩토링

--- analysis/DCT175_finda/user_trend.py
+++ analysis/DCT175_finda/user_trend.py
@@ -10,16 +10,30 @@ raw_df = prep.raw_data_concat(media_filter=['Facebook','Facebook Ads','Facebook_
                          to_date = info.to_date)
 
 raw_df_exception = prep.campaign_name_exception(raw_df)
-raw_df_exception['campaign'] = raw_df_exception['campaign'].str.lower()
 
-campaign_cost_df = prep.get_campaign_cost_df(info.raw_dir, from_date = info.from_date, to_date = info.to_date)
-campaign_cost_df['campaign'] = campaign_cost_df['campaign'].str.lower()
-cost_df_pivot = campaign_cost_df.pivot_table(index = ['media_source', 'campaign'], values = 'cost', aggfunc = 'sum').reset_index()
+loan_users = list(raw_df_exception.loc[raw_df_exception['event_name'] == 'loan_contract_completed', 'appsflyer_id'].unique())
+raw_df_loan_user_filter = raw_df_exception.loc[raw_df_exception['appsflyer_id'].isin(loan_users)]
+raw_df_loan_user_filter = raw_df_loan_user_filter.sort_values('event_time')
 
-# campaign_cost - raw_data merge
-raw_pivot = raw_df_exception.pivot_table(index=['media_source','campaign'], columns='event_name', values='event_time', aggfunc='count').reset_index().fillna(0)
-merged_df = pd.merge(cost_df_pivot, raw_pivot, how='outer', on=['media_source', 'campaign'])
+# install data
+install_data = raw_df_loan_user_filter.loc[raw_df_loan_user_filter['event_name']=='install']
+install_data = install_data.drop_duplicates('appsflyer_id')
+install_data['install_date'] = install_data['install_time'].dt.date
+install_data['install_hour'] = install_data['install_time'].dt.hour
+install_data = install_data[['install_date', 'install_hour', 'appsflyer_id']]
 
 # 대출 실행 유저 기준 중복 이벤트 제거
-loan_user_events = raw_df_exception.loc[raw_df['appsflyer_id'].isin(raw_df.loc[raw_df['event_name']=='loan_contract_completed'].appsflyer_id.unique())].sort_values(by=['appsflyer_id','event_time']).drop_duplicates(['appsflyer_id','event_name'], keep='first')
-loan_user_events.to_csv(dr.download_dir + '/loan_user_events.csv', encoding='utf-8', index=False)
+target_event = ['Viewed LA Home', 'loan_contract_completed']
+target_event_data = raw_df_loan_user_filter.loc[raw_df_loan_user_filter['event_name'].isin(target_event)]
+target_event_data = target_event_data.merge(install_data, on = 'appsflyer_id', how = 'left')
+target_event_data = target_event_data.sort_values(['appsflyer_id', 'event_time'])
+
+# 첫 이벤트 여부 확인 컬럼 생성
+first_target_event_data = target_event_data.drop_duplicates(['event_name', 'appsflyer_id'], keep='first')
+first_target_event_data = first_target_event_data[['appsflyer_id', 'event_name', 'event_time']]
+first_target_event_data = first_target_event_data.rename(columns = {'event_time' : 'first_event_time'})
+
+target_event_data_featured = target_event_data.merge(first_target_event_data, on = ['appsflyer_id', 'event_name'], how = 'left')
+target_event_data_featured.loc[(target_event_data_featured['event_time']==target_event_data_featured['first_event_time']), 'is_first_event'] = True
+target_event_data_featured['is_first_event'] = target_event_data_featured['is_first_event'].fillna(False)
+target_event_data_featured.to_csv(dr.download_dir + '/loan_user_events.csv', encoding='utf-8', index=False)
