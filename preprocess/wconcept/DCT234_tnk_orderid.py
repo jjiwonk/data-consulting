32c87233382b75aa31717150c89ff5027aa5ae2b
민정수 <jsmin@madup.com>
2022-10-13 18:29:15 +0900

    fix(DCT234) : 코드 리팩토링

--- preprocess/wconcept/DCT234_tnk_orderid.py
+++ preprocess/wconcept/DCT234_tnk_orderid.py
@@ -3,9 +3,10 @@ import setting.report_date as rdate
 import pandas as pd
 import os
 import datetime
+import json
 
 raw_dir = dr.dropbox_dir + '/광고사업부/데이터컨설팅/데이터 분석 프로젝트/W컨셉/tnk_raw'
-result_dir = raw_dir
+result_dir = dr.download_dir
 
 file_path = raw_dir
 file_list = os.listdir(file_path)
@@ -16,17 +17,15 @@ for file in file_list:
 
 raw_data = raw_data.loc[raw_data['media_source']=='tnk_int']
 raw_data = raw_data.loc[raw_data['event_name']=='item_payment']
-raw_data['order_id'] = raw_data['event_value'].apply(lambda x:x.split('af_order_no')[-1].replace('"', '').replace('}', '').replace(']','').replace(':', '').split(',')[0] if x.find('af_order_no') != -1 else x)
+raw_data['order_id'] = raw_data['event_value'].apply(lambda x:json.loads(x)['af_order_no'])
 
 #ctit,itet 계산
+raw_data['attributed_touch_time'] = pd.to_datetime(raw_data['attributed_touch_time'])
+raw_data['install_time'] = pd.to_datetime(raw_data['install_time'])
+raw_data['event_time'] = pd.to_datetime(raw_data['event_time'])
 
-raw_data['attributed_touch_time'] = [datetime.datetime.strptime(i, '%Y-%m-%d %H:%M:%S') for i in raw_data['attributed_touch_time']]
-raw_data['install_time'] = [datetime.datetime.strptime(i, '%Y-%m-%d %H:%M:%S') for i in raw_data['install_time']]
-raw_data['event_time'] = [datetime.datetime.strptime(i, '%Y-%m-%d %H:%M:%S') for i in raw_data['event_time']]
+raw_data = raw_data.loc[(raw_data['install_time'] - raw_data['attributed_touch_time']) < datetime.timedelta(days=1)]
+raw_data = raw_data.loc[(raw_data['event_time'] - raw_data['install_time']) < datetime.timedelta(days=1)]
 
-raw_data = raw_data.loc[(raw_data['install_time'] - raw_data['attributed_touch_time']) <= datetime.timedelta(days=1)]
-raw_data = raw_data.loc[(raw_data['event_time'] - raw_data['install_time']) <= datetime.timedelta(days=1)]
-
-tnk_orderid = raw_data['order_id']
-
+tnk_orderid = raw_data[['event_time','order_id']]
 tnk_orderid.to_csv(result_dir + f'/wconcept_orderid_{rdate.yearmonth}.csv', index=False, encoding='utf-8-sig')
