71cb7a8a4cb35ac8e4a5b7210732c3768cd39a4d
jhha0122 <jhha@madup.com>
2022-10-27 10:37:17 +0900

    feat(tableau): sankey 데이터 파이프라인 구성

--- setting/report_date.py
+++ setting/report_date.py
@@ -1,8 +1,13 @@
 import datetime
 
-today = datetime.date.today()
-#today = datetime.date(year = 2022, month = 8, day = 1)
+# today = datetime.date.today()
+today = datetime.date(year = 2022, month = 10, day = 1)
 day_1 = today - datetime.timedelta(1)
 start_day = datetime.date(year=day_1.year, month=day_1.month, day=1)
+
 yearmonth = day_1.strftime('%Y%m')
 month_name = str(day_1.month) + '월'
+
+# for sankey_preprocess
+from_date = datetime.date(2022, 7, 1)
+to_date = datetime.date(2022, 9, 30)
