b3286d09109794f5e89dcc9a92aca2827b04a745
jjiwonk <jw.kim@madup.com>
2022-10-28 11:24:31 +0900

    fix(DCT234): w컨셉 tnk raw 변경으로 인한 컬럼명 수정

--- preprocess/wconcept/DCT234_tnk_orderid.py
+++ preprocess/wconcept/DCT234_tnk_orderid.py
@@ -15,23 +15,26 @@ for file in file_list:
     file_df = pd.read_csv(file_path + '/' + file, header=0, encoding='utf-8-sig')
     raw_data = pd.concat([raw_data, file_df])
 
-raw_data = raw_data.loc[raw_data['media_source']=='tnk_int']
-raw_data.to_csv(result_dir + f'/wconcept_tnkraw_{rdate.yearmonth}.csv', index=False, encoding='utf-8-sig')
-raw_data = raw_data.loc[raw_data['event_name']=='item_payment']
+raw_data = raw_data.loc[raw_data['Media Source']=='tnk_int']
+#raw_data.to_csv(result_dir + f'/wconcept_tnkraw_{rdate.yearmonth}.csv', index=False, encoding='utf-8-sig')
+#raw_data = raw_data.loc[raw_data['event_name']=='item_payment']
 #raw_data['order_id'] = raw_data['event_value'].apply(lambda x:json.loads(x)['af_order_no'])
-raw_data['order_id'] = raw_data['event_value'].apply(lambda x:x.split('af_order_no')[-1].replace('"', '').replace('}', '').replace(']', '').replace(':', '').split(',')[0] if x.find(
+raw_data['주문번호'] = raw_data['Event Value'].apply(lambda x:x.split('af_order_no')[-1].replace('"', '').replace('}', '').replace(']', '').replace(':', '').split(',')[0] if x.find(
     'af_order_no') != -1 else x )
+raw_data.loc[raw_data['Event Name'] != 'item_payment','주문번호'] = '-'
 
 #ctit,itet 계산
-raw_data['attributed_touch_time'] = pd.to_datetime(raw_data['attributed_touch_time'])
-raw_data['install_time'] = pd.to_datetime(raw_data['install_time'])
-raw_data['event_time'] = pd.to_datetime(raw_data['event_time'])
+raw_data['Attributed Touch Time'] = pd.to_datetime(raw_data['Attributed Touch Time'])
+raw_data['Install Time'] = pd.to_datetime(raw_data['Install Time'])
+raw_data['Event Time'] = pd.to_datetime(raw_data['Event Time'])
 
-raw_data = raw_data.loc[(raw_data['install_time'] - raw_data['attributed_touch_time']) < datetime.timedelta(days=1)]
-raw_data = raw_data.loc[(raw_data['event_time'] - raw_data['install_time']) < datetime.timedelta(days=1)]
+raw_data = raw_data.loc[(raw_data['Install Time'] - raw_data['Attributed Touch Time']) < datetime.timedelta(days=1)]
+raw_data = raw_data.loc[(raw_data['Event Time'] - raw_data['Install Time']) < datetime.timedelta(days=1)]
 
-tnk_orderid = raw_data[['event_time','order_id']]
-tnk_orderid = tnk_orderid.sort_values(by=['event_time'] ,ascending= True)
-tnk_orderid = tnk_orderid.drop_duplicates(subset = 'order_id', keep='first')
+#raw_data = raw_data.drop_duplicates(subset = '주문번호', keep='first')
 
-tnk_orderid.to_csv(result_dir + f'/wconcept_orderid_{rdate.yearmonth}.csv', index=False, encoding='utf-8-sig')
+#tnk_orderid = raw_data[['event_time','order_id']]
+#tnk_orderid = tnk_orderid.sort_values(by=['event_time'] ,ascending= True)
+#tnk_orderid = tnk_orderid.drop_duplicates(subset = 'order_id', keep='first')
+
+raw_data.to_csv(result_dir + f'/wconcept_orderid_{rdate.yearmonth}.csv', index=False, encoding='utf-8-sig')
