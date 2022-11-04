da7c7087dc4fb1d67aa2e503e1aeb7d425cc10f4
jhha0122 <jhha@madup.com>
2022-11-04 14:30:44 +0900

    fix(DCT283): raw_data_union.py 확장자 확인 코드 추가

--- solution/DCT283_local_to_googlesheet/raw_data_union.py
+++ solution/DCT283_local_to_googlesheet/raw_data_union.py
@@ -13,9 +13,15 @@ def create_folder(dir):
 def union_raw_data(raw_dir, result_dir, file_name):
     total_df = pd.DataFrame()
     raw_files = os.listdir(raw_dir)
-    for file in raw_files:
-        temp = pd.read_excel(raw_dir + '/' + file)
-        total_df = pd.concat([total_df, temp], axis=0)
+    data_type = raw_files[0][-4:]
+    if data_type == 'xlsx':
+        for file in raw_files:
+            temp = pd.read_excel(raw_dir + '/' + file)
+            total_df = pd.concat([total_df, temp], axis=0)
+    else:
+        for file in raw_files:
+            temp = pd.read_csv(raw_dir + '/' + file)
+            total_df = pd.concat([total_df, temp], axis=0)
     create_folder(result_dir)
     total_df.to_excel(result_dir + '/' + file_name, index=False, encoding='utf-8-sig')
     print('union successful')
