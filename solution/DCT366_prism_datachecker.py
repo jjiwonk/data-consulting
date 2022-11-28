2a4cc4c42875df59419b6a68dde9d27479adfcb0
민정수 <jsmin@madup.com>
2022-11-28 18:29:46 +0900

    fix(DCT366) : 수정 의견 첨부

--- solution/DCT366_prism_datachecker.py
+++ solution/DCT366_prism_datachecker.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 #정합성 체크를 할 데이터 불러오기
 
-def sheet_raw(x):
+def sheet_raw(x): # 변수 이름은 조금 더 직관적인 것을 사용해주시는게 좋을 것 같습니다! x -> sheet_name 요런식으로요
     sheet = sp.spread_document_read(
         'https://docs.google.com/spreadsheets/d/11-TknpD_HzxZ-N7sVEJrS7iEkJcUz4xzJVL0YQPrvZY/edit#gid=0')
+    # 함수 내에서 시트를 불러오고 있는데 요렇게 되면 시트를 세번 불러오게 됩니다.
+    # 시트 불러오는 부분을 바깥으로 빼내고 시트를 인수로 전달하는 방식이 조금 더 효율적일 것 같아요
     df = sp.spread_sheet(sheet, x)
     return df
 
@@ -32,7 +34,7 @@ df2_piv = get_pivot(df2,'api')
 
 # api(프리즘) 컬럼 리스트와 매체 컬럼 리스트 정리
 
-def col_rule(x):
+def col_rule(x): # 요 부분은 간단한 내용이라 함수로 만들지 않아도 될 것 같아요!
     rep_col = df_col['rep']
     api_col = df_col['api']
     col_dic = dict(zip(rep_col,api_col))
@@ -45,7 +47,7 @@ df2_piv = df2_piv[col_name]
 
 #데이터간 비교
 
-def compare(df, df2):
+def compare(df, df2): # iloc 사용하는 경우 연산 속도가 많이 느려져서 아래에 대안적인 방법으로 소개드립니다
 
     mismat = df.copy()
     for i in range(len(df2.index)):
@@ -56,7 +58,28 @@ def compare(df, df2):
 
 df_f =compare(df_piv, df2_piv)
 
+# 일부 내용 수정한 ver 2 참고 부탁드립니다
 
+sheet = sp.spread_document_read('https://docs.google.com/spreadsheets/d/11-TknpD_HzxZ-N7sVEJrS7iEkJcUz4xzJVL0YQPrvZY/edit#gid=0')
+def sheet_raw_ver2(sheet, sheet_name): # 변수 이름은 조금 더 직관적인 것을 사용해주시는게 좋을 것 같습니다! x -> sheet_name 요런식으로요
+    df = sp.spread_sheet(sheet, sheet_name)
+    return df
+
+df_col = sheet_raw_ver2(sheet, 'columns')
+df_col = df_col.loc[df_col['field']!='none']
+column_dict = dict(zip(list(df_col['rep']), list(df_col['api'])))
+
+dimension_list = df_col.loc[df_col['field'] == 'dimension', 'api'].to_list()
+metric_list = df_col.loc[df_col['field'] == 'metric', 'api'].to_list()
+
+df = sheet_raw_ver2(sheet, 'RD')
+df = df.rename(columns = column_dict)
+
+df2 = sheet_raw_ver2(sheet, 'RD2')
+df2[metric_list] = df2[metric_list].apply(lambda x : float(x) * -1) # value 들에 -1 곱해줌
+
+df_concat = pd.concat([df, df2], sort=False, ignore_index= True)
+df_pivot = df_concat.pivot_table(index = dimension_list, values = metric_list, aggfunc = 'sum').reset_index() # 데이터 합친다음 피버팅하는데 값이 같았다면 두개 더해서 0이 나올 것
 
 
 
