d0775b6585f38b8a8e2a0aaa0b248863043ec88f
jhha0122 <jhha@madup.com>
2022-12-14 18:09:27 +0900

    feat(batch): 배치잡 테스트용 실행파일 추가

--- solution/batch_execute.py
+++ solution/batch_execute.py
@@ -1,6 +1,6 @@
 import batch_test
 import setting.ec2_directory as dr
-from spreadsheet import spreadsheet
+import spreadsheet.ec2_spreadsheet as spreadsheet
 
 doc = spreadsheet.spread_document_read('https://docs.google.com/spreadsheets/d/14i42NrpnA_9k8nCgyc4Y5KssLP8tOOXUyzYw0un7qXY/edit#gid=211027705')
 ADVERTISER = '무신사'
