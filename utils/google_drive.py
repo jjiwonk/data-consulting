be466eb9ce1e68bd95660c925835c2f1f7b8d714
hyeonchan <hckim@madup.com>
2022-12-22 00:44:29 +0900

    feat(utils): WorkSheet 객체 만들어 df 사용 용이하도록 변경
    gspread.worksheet.Worksheet를 상속받은 WorkSheet 객체를 만들어 DataFrame으로 값을 가져오거나
    삽입할 수 있는 기능을 추가함.

--- utils/google_drive.py
+++ utils/google_drive.py
@@ -2,7 +2,9 @@ import os
 import time
 
 import gspread
+from gspread.utils import finditem
 from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
 
 from utils.const import *
 from utils.path_util import get_tmp_path
@@ -11,6 +13,19 @@ from utils.s3 import download_file
 
 RETRY_CNT = 5
 WAITING_TIME = 15
+
+
+class WorkSheet(gspread.worksheet.Worksheet):
+    def __init__(self, spreadsheet, properties):
+        super().__init__(spreadsheet, properties)
+
+    def get_df_values(self, row: int = 0, col: int = 0) -> pd.DataFrame:
+        values = self.get_all_values()
+        return pd.DataFrame(values, columns=values[row]).iloc[row + 1:, col:]
+
+    def insert_df(self, data: pd.DataFrame, row: int = 1):
+        values = [row.to_list() for row in data.iloc]
+        self.insert_rows(values=values, row=row)
 
 
 class GoogleDrive:
@@ -47,12 +62,17 @@ class GoogleDrive:
                 raise e
         return spread_sheet
 
-    def get_work_sheet(self, url, sheet_name) -> gspread.worksheet.Worksheet:
+    def get_work_sheet(self, url, sheet_name) -> WorkSheet:
         spread_sheet = self.get_spread_sheet(url)
         work_sheet = None
         for i in range(RETRY_CNT):
             try:
-                work_sheet = spread_sheet.worksheet(sheet_name)
+                sheet_data = spread_sheet.fetch_sheet_metadata()
+                item = finditem(
+                    lambda x: x["properties"]["title"] == sheet_name,
+                    sheet_data["sheets"],
+                )
+                work_sheet = WorkSheet(spread_sheet, item["properties"])
                 break
             except gspread.exceptions.APIError as e:
                 error_code = e.args[0].get("code")
