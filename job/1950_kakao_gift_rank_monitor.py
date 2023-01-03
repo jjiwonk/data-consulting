d19f06d54c9e64a20a678e21eaf7c152b7281dfa
jhha0122 <jhha@madup.com>
2023-01-03 16:10:43 +0900

    feat(job): 1950_kakao_gift_rank_monitor.py BB지원 솔루션 1950 실행파일 추가

---
+++ job/1950_kakao_gift_rank_monitor.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from solution.kakao_gift_ranking_collect import KakaoGiftRankingCollect
+from solution.kakao_gift_ranking_collect import Key
+from utils.slack_bot import WEBHOOK_URL
+
+if __name__ == "__main__":
+    test_set = dict(
+        brand_name="1950",
+        slack_webhook_url=WEBHOOK_URL['bb_1950_카카오선물하기_랭킹모니터링'],
+        spreadsheet_url="https://docs.google.com/spreadsheets/d/109SDT_tbvGRPOs0zfRDhLbD9cR-wIfkKllkfj6mMicQ/edit#gid=0",
+        sheet_name = "시트1",
+        # slack_mention_id="UN9Q07FQB",
+        search_keywords="1950, 치약",
+    )
+    test_attr = dict(
+        schedule_time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), owner_id="bb", product_id="1950"
+    )
+
+    Key.USE_HEADLESS = False
+    # from madup_argo.core.util.log import change_level_info
+
+    # change_level_info()
+    worker = KakaoGiftRankingCollect(test_attr, test_set)
+    print(worker.do_work(test_attr, test_set))
