e333d2264fc51abf5f4b4369070d0f0c2545afb0
jhha0122 <jhha@madup.com>
2023-01-09 18:42:19 +0900

    feat(job): 네이버 SA 모니터링 잡 실행파일 생성(1950, 라티브)

---
+++ job/toothpaste_keyword_naver_ad_monitor.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from solution.naver_keyword_ad_monitor import NaverKeywordAdMonitor
+from solution.naver_keyword_ad_monitor import Key
+from utils.slack_bot import WEBHOOK_URL
+
+if __name__ == "__main__":
+    test_set = dict(
+        keyword="치약",
+        powerlink_ad_cnt_pc="5",
+        powerlink_ad_cnt_mo="3",
+        slack_webhook_url=WEBHOOK_URL['bb_1950_네이버sa_모니터링'],
+        spreadsheet_url="https://docs.google.com/spreadsheets/d/1i-f55AOXVBNfUKlHnfc8_jw54Kc2s3D3B-fsWSVoSvM/edit#gid=0",
+    )
+    test_attr = dict(
+        schedule_time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), owner_id="bb", product_id="bb"
+    )
+    # Key.USE_HEADLESS = False
+    worker = NaverKeywordAdMonitor(test_attr, test_set)
+    print(worker.do_work(test_attr, test_set))
