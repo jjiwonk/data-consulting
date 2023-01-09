06be4ef4dd698af7e13e644ca91cc295ee6562f1
jhha0122 <jhha@madup.com>
2023-01-09 18:42:41 +0900

    fix(utils): slack_bot.py URL 추가

--- utils/slack_bot.py
+++ utils/slack_bot.py
@@ -3,5 +3,7 @@ WEBHOOK_URL = {
     'bb_latib_매출_모니터링': 'https://hooks.slack.com/services/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',
     '1950_cafe24_매출_모니터링': 'https://hooks.slack.com/services/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',
     '1950_카카오선물하기_매출_모니터링': 'https://hooks.slack.com/services/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',
-    'bb_1950_카카오선물하기_랭킹모니터링': 'https://hooks.slack.com/services/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'
+    'bb_1950_카카오선물하기_랭킹모니터링': 'https://hooks.slack.com/services/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',
+    'bb_latib_네이버sa_모니터링': 'https://hooks.slack.com/services/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',
+    'bb_1950_네이버sa_모니터링': 'https://hooks.slack.com/services/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'
 }
