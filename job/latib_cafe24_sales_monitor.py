db554ec37156359d309acda954a5116253d7e6d5
jhha0122 <jhha@madup.com>
2023-01-19 13:58:10 +0900

    fix(job): latib_cafe24_sales_monitor.py 실행 파일 수정 (로깅 에러 확인 필요)

--- job/latib_cafe24_sales_monitor.py
+++ job/latib_cafe24_sales_monitor.py
@@ -1,29 +1,22 @@
 from datetime import datetime
 
 from solution.cafe24_sales_monitor import Cafe24SalesMonitor
-from solution.cafe24_sales_monitor import Key
 from utils.slack_bot import WEBHOOK_URL
 
 
 if __name__ == "__main__":
-    set = dict(
-        cafe24_id="latib",
-        cafe24_pw="fkxlqm2022*!",
-        store_name="latib",
-        # monitor_detail='false',
-        slack_webhook_url=WEBHOOK_URL['bb_latib_매출_모니터링'],
-        # slack_mention_id="UN9Q07FQB",
-        spreadsheet_url="https://docs.google.com/spreadsheets/d/1I1J4LStGRKyT_TUE-cFXon9DZwt4kYg_V69orgRcSPI/edit#gid=0",
-        raw_sheet_name="RD",
-        sales_sheet_name="Summary",
-        # n_products="10"
-    )
+    worker = Cafe24SalesMonitor(__file__)
     attr = dict(
         schedule_time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), owner_id="bb", product_id="latib"
     )
-    # Key.USE_HEADLESS = False
-    # from madup_argo.core.util.log import change_level_info
+    info = dict(
+        cafe24_id="latib",
+        cafe24_pw="fkxlqm2022*!",
+        store_name="latib",
+        slack_webhook_url=WEBHOOK_URL['bb_latib_매출_모니터링'],
+        spreadsheet_url="https://docs.google.com/spreadsheets/d/1I1J4LStGRKyT_TUE-cFXon9DZwt4kYg_V69orgRcSPI/edit#gid=0",
+        raw_sheet_name="RD",
+        sales_sheet_name="Summary"
+    )
 
-    # change_level_info()
-    worker = Cafe24SalesMonitor(attr, set)
-    print(worker.do_work(attr, set))
+    worker.work(attr=attr, info=info)
