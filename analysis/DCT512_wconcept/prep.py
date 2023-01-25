944345dbc48bc6d2b8cdab1b378435b030154f74
jjiwonk <jw.kim@madup.com>
2023-01-25 19:47:54 +0900

    fix(DCT512): W컨셉 LTV 분석 데이터 전처리 수정

--- analysis/DCT512_wconcept/prep.py
+++ analysis/DCT512_wconcept/prep.py
@@ -7,8 +7,10 @@ import pyarrow as pa
 import pyarrow.csv as pacsv
 import os
 import json
+import setting.directory as dr
 
 file_dir = info.paid_dir
+#file_dir = dr.dropbox_dir + '/광고사업부/4. 광고주/W컨셉/리포트 자동화/appsflyer/~ 2204'
 
 def data_read():
     dtypes = {
@@ -19,7 +21,8 @@ def data_read():
         'media_source': pa.string(),
         'campaign': pa.string(),
         'ad' : pa.string(),
-        'event_value': pa.string()}
+        'event_value': pa.string(),
+        'appsflyer_id':pa.string() }
 
     index_columns = list(dtypes.keys())
     convert_ops = pacsv.ConvertOptions(column_types=dtypes, include_columns=index_columns)
@@ -40,5 +43,16 @@ def data_read():
 
 df = data_read()
 
+df['order_id'] = df['event_value'].apply(lambda x:x.split('af_order_no')[-1].replace('"', '').replace('}', '').replace(']', '').replace(':', '').replace('\\','').split(',')[0] if x.find(
+    'af_order_no') != -1 else '-' )
 
+df = df.loc[df['order_id'] != '-']
+df = df.loc[df['order_id'] != '']
+
+df = df.sort_values(by = ['appsflyer_id','event_time'])
+df2 = df.drop_duplicates('appsflyer_id',keep='first')
+
+df2 = df2[['event_time','appsflyer_id','order_id']]
+
+df2.to_csv(dr.dropbox_dir+'/광고사업부/데이터컨설팅/데이터 분석 프로젝트/W컨셉/DCT512/raw/첫구매_orderid.csv', index= False , encoding= 'utf-8-sig')
 
