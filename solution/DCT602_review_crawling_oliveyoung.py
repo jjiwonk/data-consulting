4af897339102b21c8ca5755847bfb19fcb4e6bdc
민정수 <jsmin@madup.com>
2023-02-10 15:21:59 +0900

    fix(DCT602) : 리뷰 ID 생성 함수 수정

--- solution/DCT602_review_crawling_oliveyoung.py
+++ solution/DCT602_review_crawling_oliveyoung.py
@@ -48,7 +48,7 @@ class parse_data :
         product_id = row['product_id']
         user_name = row['user_name']
         review_date = row['review_date']
-        message = "{}.{}.{}".format(platform, user_name, review_date)
+        message = "{}.{}.{}.{}".format(platform, user_name,product_id,review_date)
         hash = hmac.new(bytes(product_id, "utf-8-"), bytes(message, "utf-8-"), hashlib.sha256)
         result = hash.hexdigest()
         return result
