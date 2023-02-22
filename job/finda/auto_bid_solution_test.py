d589977afd28e02b286d9020bb10db806c4cba35
jhha0122 <jhha@madup.com>
2023-02-22 15:18:44 +0900

    fix(job): 입찰 솔루션 서버 테스트 용 배치 실행 파일

--- job/finda/auto_bid_solution_test.py
+++ job/finda/auto_bid_solution_test.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from solution.DCT697_auto_bid_solution import AutoBidSolution
 
 if __name__ == "__main__":
@@ -11,7 +9,7 @@ if __name__ == "__main__":
         media_info='네이버SA',
         use_headless=True,
         spread_sheet_url="https://docs.google.com/spreadsheets/d/1Qk3f7FjPDeOK8hEwAp_PV6TYWjRzf6YclM_1YBqU0p0/edit#gid=0",
-        keyword_sheet="키워드 설정",
+        keyword_sheet="키워드 설정(TEST)",
         keyword_column="키워드",
         ad_names='["finda", "핀다"]',
         customer_id=2503399,
