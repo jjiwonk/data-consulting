0b8ff5204e20c67ff47f33d4d03e53bea69bb8c2
민정수 <jsmin@madup.com>
2023-02-22 15:08:49 +0900

    fix(DCT421) : 비즈보드 소재 배너 사이즈 확장 대응

--- solution/DCT421_bizboard_creative_maker.py
+++ solution/DCT421_bizboard_creative_maker.py
@@ -6,7 +6,7 @@ class directory :
     dropbox_dir = dr.dropbox_dir
     font_dir = dr.dropbox_dir + '/광고사업부/데이터컨설팅/데이터 솔루션/비즈보드 소재 자동화/font'
 class info :
-    account_name = '광고주명'
+    account_name = '29CM'
 
     spread_url = 'https://docs.google.com/spreadsheets/d/1hFBpfIPWNuIXu4T8-5tqI7d32Ypr4TuQar21IHzc74A/edit#gid=0'
     biz_doc = spreadsheet.spread_document_read(spread_url)
@@ -19,37 +19,53 @@ class info :
         '일반형': {
             '한줄형': {
                 'main_text_x': 48,
-                'main_text_y': 81,
+                'main_text_y': 106,
             },
             '두줄형': {
                 'main_text_x': 48,
-                'main_text_y': 48,
+                'main_text_y': 77,
                 'sub_text_x': 48,
-                'sub_text_y': 111
+                'sub_text_y': 144
             }
         },
         '앱고지형':  {
             '한줄형': {
-                'app_text_x' : 90,
-                'app_text_y' : 59,
+                'app_text_x' : 95,
+                'app_text_y' : 78,
                 'main_text_x': 48,
-                'main_text_y': 95,
+                'main_text_y': 127,
             },
             '두줄형': {
-                'app_text_x': 90,
-                'app_text_y': 34,
+                'app_text_x': 95,
+                'app_text_y': 50,
                 'main_text_x': 48,
-                'main_text_y': 70,
+                'main_text_y': 99,
                 'sub_text_x': 48,
-                'sub_text_y': 133
+                'sub_text_y': 163
             }
         }
     }
 
 class font :
-    main_font = ImageFont.truetype(directory.font_dir + '/Spoqa Han Sans Bold.ttf', size=45)
-    sub_font = ImageFont.truetype(directory.font_dir + '/Spoqa Han Sans Regular.ttf', size=36)
+    main_font = ImageFont.truetype(directory.font_dir + '/Spoqa Han Sans Bold.ttf', size=48)
+    sub_font = ImageFont.truetype(directory.font_dir + '/Spoqa Han Sans Regular.ttf', size=39)
     app_font = ImageFont.truetype(directory.font_dir + '/Spoqa Han Sans Regular.ttf', size=24)
+
+class banner :
+    def __init__(self, default_image):
+        self.default_image = default_image
+        self.imdraw = ImageDraw.Draw(default_image)
+    def get_real_y_of_text(self, object_y, target_font, text):
+        object_height = target_font.getmask(text).getbbox()[3]
+        actual_height = target_font.getmetrics()[0] + target_font.getmetrics()[1]
+
+        result = object_y - ((actual_height - object_height) / 2)
+
+        return result
+
+    def draw_text(self, x , y, input_text, font, color):
+        y = self.get_real_y_of_text(y,font, input_text)
+        self.imdraw.text((x, y), input_text, color, font)
 
 
 def update_result(row_num, text):
@@ -59,56 +75,50 @@ def update_result(row_num, text):
 def create_biz_banner(cre_info_dict):
     text_info = info.pixel_dict[cre_info_dict['배너 타입']][cre_info_dict['num_of_line']]
 
-    default_image = Image.new('RGBA', (1029, 222))
-    imdraw = ImageDraw.Draw(default_image)
+    default_image = Image.new('RGBA', (1029, 258))
+    biz_banner = banner(default_image)
 
     if cre_info_dict['배너 타입'] == '앱고지형':
         # 앱 로고 이미지
         app_logo_image = Image.open(directory.dropbox_dir + cre_info_dict['앱 로고 경로']).convert("RGBA")
 
         if cre_info_dict['num_of_line'] == '한줄형' :
-            app_y = 62
+            app_y = 71
         elif cre_info_dict['num_of_line'] == '두줄형' :
-            app_y = 37
-        default_image.paste(app_logo_image, (48, app_y), app_logo_image)
+            app_y = 43
+        biz_banner.default_image.paste(app_logo_image, (48, app_y), app_logo_image)
 
         # 앱 랜딩 고지문
-        x = text_info['app_text_x']
-        y = text_info['app_text_y']
-        imdraw.text((x, y), cre_info_dict['앱 랜딩 고지문'], (119, 119, 119), font.app_font)
-        arrow_x = default_image.getbbox()[2]
+        biz_banner.draw_text(x = text_info['app_text_x'], y = text_info['app_text_y'], input_text = cre_info_dict['앱 랜딩 고지문'], font = font.app_font, color = (119, 119, 119))
+        arrow_x = biz_banner.default_image.getbbox()[2]
 
         # 화살표 넣기
         arrow = Image.open(directory.dropbox_dir + '/광고사업부/데이터컨설팅/데이터 솔루션/비즈보드 소재 자동화/object/비즈보드_화살표.png')
-        default_image.paste(arrow, (arrow_x + 6, app_y + 10), arrow)
+        biz_banner.default_image.paste(arrow, (arrow_x + 6, app_y + 13), arrow)
 
-    x = text_info['main_text_x']
-    y = text_info['main_text_y']
-    imdraw.text((x, y), cre_info_dict['메인문구'], (76, 76, 76), font.main_font)
+    biz_banner.draw_text(x = text_info['main_text_x'], y = text_info['main_text_y'], input_text = cre_info_dict['메인문구'], font = font.main_font, color = (76, 76, 76))
 
     if cre_info_dict['num_of_line'] == '두줄형' :
-        x = text_info['sub_text_x']
-        y = text_info['sub_text_y']
-        imdraw.text((x, y), cre_info_dict['서브문구'], (119, 119, 119), font.sub_font)
+        biz_banner.draw_text(x=text_info['sub_text_x'], y=text_info['sub_text_y'], input_text=cre_info_dict['서브문구'],
+                             font=font.sub_font, color=(119, 119, 119))
 
     object_image = Image.open(directory.dropbox_dir + cre_info_dict['오브제 이미지 경로'])
 
-    default_image.paste(object_image, (666, 0), object_image)
-    default_image.save(directory.dropbox_dir + cre_info_dict['파일 저장 경로'] + '/' + cre_info_dict['저장 파일명'])
-    return default_image
+    biz_banner.default_image.paste(object_image, (666, 0), object_image)
+    biz_banner.default_image.save(directory.dropbox_dir + cre_info_dict['파일 저장 경로'] + '/' + cre_info_dict['저장 파일명'])
+    return biz_banner.default_image
 def minimum_constraint_test(test_image):
-    crop_image = test_image.crop((290, 0, 585, 222))
+    crop_image = test_image.crop((290, 0, 585, 258))
     crop_image_bbox = crop_image.getbbox()
     return crop_image_bbox == None
 
 def maximum_constraint_test(test_image):
-    crop_image = test_image.crop((585, 0, 665, 222))
+    crop_image = test_image.crop((585, 0, 665, 258))
     crop_image_bbox = crop_image.getbbox()
     return crop_image_bbox != None
 
 def do_work():
     creative_data = info.creative_data.reset_index()
-
     for i in range(len(creative_data)):
         row = creative_data.iloc[i]
         cre_info_dict = row.to_dict()
@@ -130,3 +140,5 @@ def do_work():
             result_text = 'FAIL : 이미지 생성 중 오류가 발생했습니다. 입력 정보를 다시 확인해주세요.'
 
         update_result(row['index'], result_text)
+
+do_work()
