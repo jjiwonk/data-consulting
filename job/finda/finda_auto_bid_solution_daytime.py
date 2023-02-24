08a93510f193832e90d2f70cfbd75cd2e68be9ce
jhha0122 <jhha@madup.com>
2023-02-24 14:54:44 +0900

    fix(job): 입찰 솔루션 실 운영 테스트 > 운영 시간별 키워드 분리를 위해 실행 파일 분리

--- job/finda/finda_auto_bid_solution_daytime.py
+++ job/finda/finda_auto_bid_solution_daytime.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         media_info='네이버SA',
         use_headless=True,
         spread_sheet_url="https://docs.google.com/spreadsheets/d/1Qk3f7FjPDeOK8hEwAp_PV6TYWjRzf6YclM_1YBqU0p0/edit#gid=0",
-        keyword_sheet="키워드 설정(TEST2)",
+        keyword_sheet="키워드 설정(주간)",
         keyword_column="키워드",
         ad_names='["finda", "핀다"]',
         customer_id=2503399,
