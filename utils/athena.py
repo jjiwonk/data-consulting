c4e0cf29c5def974b5547f5a30cc5a81521d05c3
민정수 <jsmin@madup.com>
2023-02-28 18:13:40 +0900

    feat(athena) : 아데나 유틸 추가

---
+++ utils/athena.py
@@ -0,0 +1,33 @@
+import boto3
+import time
+
+def athena_table_refresh(database, table_name):
+    athena = boto3.client('athena')
+    res = athena.start_query_execution(
+        QueryString=f"MSCK REPAIR TABLE {database}.{table_name}",
+        QueryExecutionContext={
+            'Database': database,
+        },
+        ResultConfiguration={
+            'OutputLocation': 's3://data-consulting-private/Unsaved/'
+        }
+    )
+
+    while True :
+        try :
+            time.sleep(5)
+            result = athena.get_query_results(QueryExecutionId=res['QueryExecutionId'])
+        except Exception as e :
+            err_response = getattr(e, 'response', None)
+            if err_response is None :
+                print(e)
+                raise(e)
+            if err_response['Error']['Message'].__contains__('Could not find results'):
+                return
+            elif err_response['Error']['Message'].__contains__('Query has not yet finished') or err_response['Error']['Message'].__contains__("Rate exceeded"):
+                time.sleep(5)
+                continue
+            print(e)
+            raise (e)
+
+        return result
