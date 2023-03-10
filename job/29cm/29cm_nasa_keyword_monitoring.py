9089ecf095b54dc4022c0442591b7bcb4b2fccb7
jhha0122 <jhha@madup.com>
2023-03-10 18:15:44 +0900

    fix(job): 29cm 키워드 모니터링 솔루션 업데이트

--- job/29cm/29cm_nasa_keyword_monitoring.py
+++ job/29cm/29cm_nasa_keyword_monitoring.py
@@ -1,4 +1,4 @@
-from solution.DCT649_keyword_monitoring_solution import KeywordMonitoring
+from solution.DCT820_keyword_monitoring_solution import KeywordMonitoring
 
 if __name__ == "__main__":
     worker = KeywordMonitoring(__file__)
@@ -8,12 +8,11 @@ if __name__ == "__main__":
     info = dict(
         media_info='네이버SA',
         use_headless=True,
-        spread_sheet_url="https://docs.google.com/spreadsheets/d/1yKaWAOY9Se0o66baOJj8t8mezkb9Va-2KOKSAD2aGoM/edit#gid=0",
-        keyword_sheet="키워드 설정",
         keyword_column="키워드",
         ad_names='["29cm", "29CM"]',
         slack_channel='dcbot_29cm_네이버sa_키워드순위모니터링',
         error_slack_channel='dcbot_29cm_네이버sa_키워드순위모니터링',
+        file_name='29cm_keywords_list.xlsx',
         send_result_msg=False
     )
 
