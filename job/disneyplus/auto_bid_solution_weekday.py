e301743dfe84854d46e6378436236a2f5e4e5708
jhha0122 <jhha@madup.com>
2023-03-14 16:38:55 +0900

    feat(job): 디즈니플러스 입찰 솔루션 배치 실행 파일 생성

---
+++ job/disneyplus/auto_bid_solution_weekday.py
@@ -0,0 +1,20 @@
+from solution.DCT697_auto_bid_solution import AutoBidSolution
+
+if __name__ == "__main__":
+    worker = AutoBidSolution(__file__)
+    attr = dict(
+        owner_id="disneyplus", channel="네이버SA"
+    )
+    info = dict(
+        media_info='네이버SA',
+        use_headless=True,
+        spread_sheet_url="https://docs.google.com/spreadsheets/d/1pQg63dqpCGBcQAi4wTaqr_2xV4OWcnBEbDUCj4TZ7cE/edit#gid=0",
+        keyword_sheet="월~목요일",
+        keyword_column="키워드",
+        ad_names='["disneyplus", "디즈니플러스"]',
+        customer_id=2384006,
+        bid_downgrade=True,
+        error_slack_channel='gl_글로벌_디즈니플러스'
+    )
+
+    worker.work(attr=attr, info=info)
