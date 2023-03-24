4076d5b257273477cf0b60c614862690f9ac7c0c
jhha0122 <jhha@madup.com>
2023-03-24 18:32:04 +0900

    fix(job): 디즈니플러스 입찰 솔루션 적용

--- job/disneyplus/auto_bid_solution_morning.py
+++ job/disneyplus/auto_bid_solution_morning.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         media_info='네이버SA',
         use_headless=True,
         spread_sheet_url="https://docs.google.com/spreadsheets/d/1pQg63dqpCGBcQAi4wTaqr_2xV4OWcnBEbDUCj4TZ7cE/edit#gid=0",
-        keyword_sheet="금~일요일",
+        keyword_sheet="일반_매일 00시~12시",
         keyword_column="키워드",
-        ad_names='["disneyplus", "디즈니플러스"]',
+        ad_names='["디즈니플러스", "disneyplus"]',
         customer_id=2384006,
         bid_downgrade=True,
         error_slack_channel='gl_글로벌_디즈니플러스'
