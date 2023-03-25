eef2c38c3c606eb6632fae1e85b9696f1d2dc77b
jhha0122 <jhha@madup.com>
2023-03-25 15:14:07 +0900

    fix(solution): 디즈니 플러스 입찰 솔루션 시트명 변경

--- job/disneyplus/auto_bid_solution_afternoon.py
+++ job/disneyplus/auto_bid_solution_afternoon.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         media_info='네이버SA',
         use_headless=True,
         spread_sheet_url="https://docs.google.com/spreadsheets/d/1pQg63dqpCGBcQAi4wTaqr_2xV4OWcnBEbDUCj4TZ7cE/edit#gid=0",
-        keyword_sheet="일반_매일 13시~ 00시",
+        keyword_sheet="일반_매일 12시~00시",
         keyword_column="키워드",
         ad_names='["디즈니플러스", "disneyplus"]',
         customer_id=2384006,
