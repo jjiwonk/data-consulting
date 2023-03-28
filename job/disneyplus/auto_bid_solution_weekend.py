d18beb6c664ea622d8f4949cabe99b920e1c40bd
jhha0122 <jhha@madup.com>
2023-03-28 10:31:09 +0900

    fix(job): 디즈니플러스 잡 추가

---
+++ job/disneyplus/auto_bid_solution_weekend.py
@@ -0,0 +1,20 @@
+from solution.DCT697_auto_bid_solution import AutoBidSolution
+
+if __name__ == "__main__":
+    worker = AutoBidSolution(__file__)
+    attr = dict(
+        owner_id="disneyplus", channel="네이버SA"
+    )
+    info = dict(
+        media_info='네이버SA',
+        use_headless=True,
+        spread_sheet_url="https://docs.google.com/spreadsheets/d/1pQg63dqpCGBcQAi4wTaqr_2xV4OWcnBEbDUCj4TZ7cE/edit#gid=2116326160",
+        keyword_sheet="브랜드_금~일요일",
+        keyword_column="키워드",
+        ad_names='["디즈니플러스", "disneyplus"]',
+        customer_id=2384006,
+        bid_downgrade=True,
+        error_slack_channel='gl_글로벌_디즈니플러스_alert'
+    )
+
+    worker.work(attr=attr, info=info)
