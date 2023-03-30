e3b495077a286ac435f8d86a1ac191ecf91b33a5
jhha0122 <jhha@madup.com>
2023-03-30 15:45:30 +0900

    feat(job): keyword_monitoring_report_athena_manually_refresh.py 아데나 키워드 모니터링 리포트 테이블 수동 리프레시 실행파일

---
+++ job/dc_team/keyword_monitoring_report_athena_manually_refresh.py
@@ -0,0 +1,19 @@
+from solution.athena_table_manually_refresh import AthenaRefresh
+
+if __name__ == "__main__":
+    worker = AthenaRefresh(__file__)
+    attr = dict(
+        owner_id="dc_team",
+        product_id="athena_refresh"
+    )
+
+    info = dict(
+        database="dc_athena",
+        table_name="keyword_monitoring_report",
+        table_s3_path="data-consulting-private/keyword_monitoring/",
+        owner_ids=['disneyplus'],
+        channel='네이버SA',
+        start_date='2023-04-30',
+    )
+
+    worker.work(attr=attr, info=info)
