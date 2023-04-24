ecb2f190b8cfc4264698c68916fc5ada53cd0806
jjiwonk <jw.kim@madup.com>
2023-04-24 10:58:57 +0900

    feat (DCT1086): 태블로 hyper 업로드 to cloud

---
+++ job/Tableau/keyword_monitoring_hyper_upload_tocloud.py
@@ -0,0 +1,98 @@
+from utils import s3
+from utils.path_util import get_tmp_path
+from utils import const
+from utils import athena
+import os
+import pandas as pd
+from datetime import datetime
+
+from solution.DCT1086_tableau_hyper_upload_tocloud import hyper_file_upload
+
+if __name__ == "__main__":
+    worker = hyper_file_upload(__file__)
+
+    def tmp_path_maker(owner_id):
+        tmp_path = get_tmp_path() + f"/{owner_id}/"
+        os.makedirs(tmp_path, exist_ok=True)
+        return tmp_path
+
+    def athena_download(owner_id, query_name):
+        # 아데나에서 쿼리를 가져온 후 그 쿼리를 사용하여 데이터 프레임 가져오기
+        tmp_path = tmp_path_maker(owner_id)
+        s3_query_path = f'query/{owner_id}/{query_name}'
+        f_path = s3.download_file(s3_path=s3_query_path, s3_bucket=const.DEFAULT_S3_PRIVATE_BUCKET, local_path=tmp_path)
+
+        f = open(f_path, 'r', encoding='utf-8-sig')
+        query = str(f.read())
+        del f
+        df = athena.get_table_data_from_athena('dc_athena', query, 's3')
+        os.remove(f_path)
+        return df
+
+    def data_prep(owner_id, num_list, text_list , report_col):
+
+        update_df = athena_download('tableau','tableau_keyword_monitoring_update_query.txt')
+        update_df = update_df.fillna(0)
+
+        # 기존 데이터와 중복 제거
+        tmp_path = tmp_path_maker(owner_id)
+        s3_query_path = f'keyword_monitoring/owner_id={owner_id}/update/tableau_keyword_monitoring_hyper.csv'
+        f_path = s3.download_file(s3_path=s3_query_path, s3_bucket=const.DEFAULT_S3_PRIVATE_BUCKET, local_path=tmp_path)
+        last_df = pd.read_csv(f_path)
+        os.remove(f_path)
+
+        df = pd.concat([last_df, update_df])
+        df['ad_rank'] = df['ad_rank'].fillna(0)
+        df = df.fillna('-')
+
+        # data type 정하기
+        df[num_list] = df[num_list].astype(int)
+        df[text_list] = df[text_list].astype(str)
+        df['date'] = pd.to_datetime(df['date'])
+        df = df[report_col]
+
+        df = df.drop_duplicates(keep='first')
+        df.index = range(len(df))
+
+        # 데이터 s3에 업로드하기
+        tmp_path = get_tmp_path() + f"/{owner_id}/"
+        file_name = 'tableau_keyword_monitoring_hyper.csv'
+        local_path = tmp_path + file_name
+        df.to_csv(local_path, encoding='utf-8-sig', index=False)
+
+        year = datetime.now().strftime('%Y')
+        month = datetime.now().strftime('%m')
+        day = datetime.now().strftime('%d')
+
+        s3.upload_file(local_path,
+                       s3_path=f'keyword_monitoring/owner_id={owner_id}/year={year}/month={month}/day={day}/{file_name}',
+                       s3_bucket=const.DEFAULT_S3_PRIVATE_BUCKET)
+        s3.upload_file(local_path,
+                       s3_path=f'keyword_monitoring/owner_id={owner_id}/update/{file_name}',
+                       s3_bucket=const.DEFAULT_S3_PRIVATE_BUCKET)
+
+        os.remove(local_path)
+
+        data = []
+        for i in range(len(df)):
+            list = df.loc[i].to_list()
+            data.append(list)
+
+        return data
+
+    attr = s3.get_info_from_s3('tableau','hyper_upload_solution_tocloud')
+
+    info = dict(
+        hyper_name ='keyword_monitoring.hyper',
+        project_name = 'RD',
+        success_alert_channel = 'pjt_dc_success',
+        error_slack_channel='pjt_dc_erro',
+        num_list = ['ad_rank','year', 'month', 'day', 'hour', 'minute'],
+        text_list = ['collected_at','pc_mobile_type', 'weekday', 'ad_name','ad_keyword','screenshot_url','url_dict', 'owner_id', 'channel'],
+        date_list = ['date']
+    )
+
+    info['report_col'] =  info['num_list'] + info['text_list'] + info['date_list']
+    info['data'] = data_prep('tableau',info['num_list'], info['text_list'], info['report_col'])
+
+    worker.work(attr=attr, info=info)
---
+++ solution/DCT1086_tableau_hyper_upload_tocloud.py
@@ -0,0 +1,126 @@
+from pathlib import Path
+import tableauserverclient as TSC
+from tableauhyperapi import HyperProcess, Telemetry, \
+    Connection, CreateMode, \
+    NOT_NULLABLE, NULLABLE, SqlType, TableDefinition, \
+    Inserter, \
+    escape_name, escape_string_literal, \
+    TableName
+from worker.abstract_worker import Worker
+
+class hyper_file_upload(Worker):
+
+    def table_type_definition(self, num_list , text_list , date_list):
+
+    # 테이블 타입 지정해주기
+        table_definition = []
+
+        for i in num_list:
+            num_td = TableDefinition.Column(name= i, type=SqlType.numeric(precision=1, scale=0), nullability=NOT_NULLABLE)
+            table_definition.append(num_td)
+
+        for i in text_list:
+            text_td = TableDefinition.Column(name=i, type=SqlType.text(), nullability=NOT_NULLABLE)
+            table_definition.append(text_td)
+
+        for i in date_list:
+            date_td = TableDefinition.Column(name=i, type=SqlType.timestamp(), nullability=NOT_NULLABLE)
+            table_definition.append(date_td)
+
+        return table_definition
+
+    def insert_data(self, hyper_name, table_definition ,data ,tableau_token_name , tableau_token , tableau_sever, project_name):
+
+        path_to_database = Path(hyper_name)
+
+        # The table is called "Extract" and will be created in the "Extract" schema
+        # and contains four columns.
+        extract_table = TableDefinition(
+            table_name=TableName("Extract", "Extract"),
+            columns=table_definition
+        )
+
+        """
+        An example demonstrating a simple single-table Hyper file including table creation and data insertion with different types
+        This code is lifted from the below example:
+        https://github.com/tableau/hyper-api-samples/blob/main/Tableau-Supported/Python/insert_data_into_single_table.py
+        """
+        print("Creating single table for publishing.")
+
+        # Starts the Hyper Process with telemetry enabled to send data to Tableau.
+        # To opt out, simply set telemetry=Telemetry.DO_NOT_SEND_USAGE_DATA_TO_TABLEAU.
+        with HyperProcess(telemetry=Telemetry.SEND_USAGE_DATA_TO_TABLEAU) as hyper:
+            # Creates new Hyper file "customer.hyper".
+            # Replaces file with CreateMode.CREATE_AND_REPLACE if it already exists.
+            with Connection(endpoint=hyper.endpoint,
+                            database=path_to_database,
+                            create_mode=CreateMode.CREATE_AND_REPLACE) as connection:
+                connection.catalog.create_schema(schema=extract_table.table_name.schema_name)
+                connection.catalog.create_table(table_definition=extract_table)
+
+                # The rows to insert into the "Extract"."Extract" table.
+                data_to_insert = data
+
+                with Inserter(connection, extract_table) as inserter:
+                    #print(data_to_insert)
+                    inserter.add_rows(rows=data_to_insert)
+                    inserter.execute()
+
+                # The table names in the "Extract" schema (the default schema).
+                table_names = connection.catalog.get_table_names("Extract")
+                print(f"Tables available in {path_to_database} are: {table_names}")
+
+                # Number of rows in the "Extract"."Extract" table.
+                # `execute_scalar_query` is for executing a query that returns exactly one row with one column.
+                row_count = connection.execute_scalar_query(query=f"SELECT COUNT(*) FROM {extract_table.table_name}")
+                print(f"The number of rows in table {extract_table.table_name} is {row_count}.")
+
+            print("The connection to the Hyper file has been closed.")
+        print("The Hyper process has been shut down.")
+
+        """
+        Shows how to leverage the Tableau Server Client (TSC) to sign in and publish an extract directly to Tableau Online/Server
+        """
+
+        # Sign in to server
+        tableau_auth = TSC.PersonalAccessTokenAuth(tableau_token_name, tableau_token)
+        server = TSC.Server(tableau_sever, use_server_version=True)
+
+        print(f"Signing into at {tableau_sever}")
+        with server.auth.sign_in(tableau_auth):
+            # Define publish mode - Overwrite, Append, or CreateNew
+            publish_mode = TSC.Server.PublishMode.Overwrite
+
+            # Get project_id from project_name
+            all_projects, pagination_item = server.projects.get()
+            for project in TSC.Pager(server.projects):
+                if project.name == project_name:
+                    project_id = project.id
+
+            # Create the datasource object with the project_id
+            datasource = TSC.DatasourceItem(project_id)
+
+            print(f"Publishing {hyper_name} to {project_name}...")
+            # Publish datasource
+            datasource = server.datasources.publish(datasource, path_to_database, publish_mode)
+            print("Datasource published. Datasource ID: {0}".format(datasource.id))
+
+            return print('Tableau Hyper Upload Success')
+
+    def do_work(self ,info:dict, attr:dict):
+
+        hyper_name = info['hyper_name']
+        tableau_token_name = attr['tableau_token_name']
+        tableau_token = attr['tableau_token']
+        tableau_sever = attr['tableau_sever']
+        project_name = info['project_name']
+
+        num_list = info['num_list']
+        date_list = info['date_list']
+        text_list = info['text_list']
+        data = info['data']
+
+        table_definition = self.table_type_definition(num_list,text_list,date_list)
+        self.insert_data(hyper_name, table_definition ,data ,tableau_token_name, tableau_token, tableau_sever, project_name)
+
+        return "Tableau Hyper File Upload Success"
