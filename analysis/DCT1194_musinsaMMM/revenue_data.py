75d1e3d63b0aba97f442e0724ac1a9f73da622ed
jjiwonk <jw.kim@madup.com>
2023-05-22 18:16:58 +0900

    fix(DCT1194) : 무탠다드 RD 업데이트

--- analysis/DCT1194_musinsaMMM/revenue_data.py
+++ analysis/DCT1194_musinsaMMM/revenue_data.py
@@ -22,7 +22,13 @@ def data_prep():
 
     data.loc[data['category'] == '상시세일즈' , 'category'] = 'sales'
     data.loc[data['category'] == '여성유입', 'category'] = 'female'
-
+    data['media'].unique()
+    # 미디어 정제
+    brand_click = data.loc[data['media'] == 'naver_brandsearch']
+    brand_click = brand_click.pivot_table(index='date', columns=['media'], values=['C'],
+                                  aggfunc='sum').reset_index().fillna(0)
+    brand_click.columns  = ['date','naverbs_click']
+    data = data.loc[data['media'].isin(['Facebook','KAKAO', 'Criteo'])]
     revenue_data = data.groupby(['date'])[['paid_revenue']].sum().reset_index()
 
     media_data = data.pivot_table(index='date', columns= ['media','category'], values=['I', 'S'], aggfunc='sum').reset_index().fillna(0)
@@ -30,6 +36,7 @@ def data_prep():
     media_data = media_data.rename(columns = {'__date' : 'date'})
 
     result_data = pd.merge(revenue_data , media_data , on ='date', how = 'left')
+    result_data = pd.merge(result_data, brand_click, on='date', how='left')
     result_data.to_csv(result_dir + '/MT_MMM_RD.csv', index =False, encoding = 'utf-8-sig')
 
     return result_data
