e539c5d88579df89ce145950e8ae534ab50f8119
민정수 <jsmin@madup.com>
2023-09-06 14:27:59 +0900

    fix(EDA) : 뮤직카우 데이터 분석 수정

--- analysis/DCT1634_musicow_general/EDA.py
+++ analysis/DCT1634_musicow_general/EDA.py
@@ -1,72 +1,14 @@
 from setting import directory as dr
-import pandas as pd
-import os
-import numpy as np
-from workers.func import user_identifier
 from workers.func import FunnelDataGenerator
 from workers.func import SankeyModeling
-import datetime
+from analysis.DCT1634_musicow_general.raw_data import data_set
 
-raw_dir = dr.download_dir + '/뮤직카우RD'
-raw_files = os.listdir(raw_dir)
-
-df_list = []
-for file in raw_files :
-    df = pd.read_csv(raw_dir + '/' + file)
-    df_list.append(df)
-
-data = pd.concat(df_list)
-data = data.sort_values(['Event Time', 'Install Time'])
-data['Media Source'] = data['Media Source'].fillna('Organic')
-data['Media Source'].value_counts()
-data['Customer User ID'] = data['Customer User ID'].fillna('')
-data['Customer User ID'] = data['Customer User ID'].astype('str')
-
-user_dict = user_identifier(data, 'AppsFlyer ID', 'Customer User ID')
-data['unique_user_id'] = data['AppsFlyer ID'].apply(lambda x : user_dict.get(x))
-
-data = data[['unique_user_id'] + list(data.columns[:-1])]
-
-# 중복 제거를 위한 우선순위 선정 : 리인게이지먼트 > UA > 오가닉 순으로 정렬
-data['dedup_order'] = data.apply(lambda x : 3 if x['Media Source'] == 'Organic' else 1 if x['Is Retargeting'] == True else 2, axis= 1)
-data = data.sort_values(['unique_user_id', 'Event Time', 'Install Time', 'dedup_order'])
-
-data[['Attributed Touch Time',
-      'Install Time',
-      'Event Time']] = data[['Attributed Touch Time', 'Install Time', 'Event Time']].apply(lambda x : pd.to_datetime(x))
+data = data_set.data
+install_user_data = data_set.install_user_data
 
 
-# Install 유저에 대해서 리타게팅 토글 켜야할지?
-install_user = set(data.loc[data['Event Name']=='install', 'unique_user_id'])
-install_user_data = data.loc[data['unique_user_id'].isin(install_user)]
-
-first_install_time = install_user_data.loc[install_user_data['Event Name']=='install'].drop_duplicates('unique_user_id', keep = 'first')
-first_install_time = first_install_time[['Install Time', 'unique_user_id', 'Media Source']]
-first_install_time = first_install_time.rename(columns = {'Install Time' : 'First Install Time', 'Media Source' : 'Install Source'})
-
-install_user_data = install_user_data.merge(first_install_time, on = 'unique_user_id', how = 'left')
-install_user_data = install_user_data.loc[install_user_data['First Install Time'] <= install_user_data['Event Time']]
-install_user_data = install_user_data.drop(
-    install_user_data.loc[(install_user_data['Event Name']=='install') &
-                          (install_user_data['Event Time']!=install_user_data['First Install Time'])].index)
-install_user_data = install_user_data.drop_duplicates(['Event Time', 'Event Name', 'unique_user_id'], keep = 'first')
-install_user_data['Event Name'].value_counts()
-
-install_user_data.index= range(len(install_user_data))
-
-# 리포팅 특성 상 중복 데이터 쌓이는 부분 검증
-# duplicate_index = install_user_data.drop_duplicates(['Event Time', 'Event Name', 'unique_user_id'], keep = 'first')
-# duplicate_data = install_user_data.loc[~install_user_data.index.isin(duplicate_index.index)]
-#
-# event_index = set(duplicate_data.index) | set(duplicate_data.index -1)
-# duplicate_event_data = install_user_data.loc[install_user_data.index.isin(event_index)]
-install_user_data['Cnt'] = 1
-install_user_pivot = install_user_data.pivot_table(index = 'Event Name', columns = 'dedup_order', values = 'Cnt', aggfunc = 'sum').reset_index()
-
-install_user_data['ITET'] = install_user_data['Event Time'] - install_user_data['First Install Time']
-install_user_data['ITET'] = install_user_data['ITET'].apply(lambda x: x.days)
-install_user_data['ITET Group'] = install_user_data['ITET'].apply(lambda x : '0d' if x == 0 else '7d' if x < 7 else '30d' if x <30 else '30d+' )
-
+install_user_pivot = install_user_data.pivot_table(index='Event Name', columns='dedup_order', values='Cnt',
+                                                   aggfunc='sum').reset_index()
 
 sign_up_event = install_user_data.loc[install_user_data['Event Name']=='af_signup_success']
 
