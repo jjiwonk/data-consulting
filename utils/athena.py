3326d35c851925a783a1a048b059b453fa4fc6dd
jhha0122 <jhha@madup.com>
2023-11-13 20:31:53 +0900

    fix(athena): athena.py 내 fetchall_athena 함수 추가 (1000행 이상의 결과값 추출)

--- utils/athena.py
+++ utils/athena.py
@@ -102,8 +102,8 @@ def get_partitions(table, path, owner_id, channel, start_date):
     )
 
 
-def get_table_data_from_athena(database, query, source= 'result'):
-    athena = boto3.client('athena', region_name = 'ap-northeast-2')
+def get_table_data_from_athena(database, query, source='result'):
+    athena = boto3.client('athena', region_name='ap-northeast-2')
     res = athena.start_query_execution(
         QueryString= query,
         QueryExecutionContext={
@@ -140,9 +140,53 @@ def get_table_data_from_athena(database, query, source= 'result'):
         os.makedirs(tmp_path, exist_ok=True)
 
         f_path = s3.download_file(s3_path=s3_path, s3_bucket=const.DEFAULT_S3_PRIVATE_BUCKET, local_path=tmp_path)
-        result_df = pd.read_csv(f_path , encoding= 'utf-8-sig')
+        result_df = pd.read_csv(f_path, encoding='utf-8-sig')
 
         os.remove(f_path)
 
     return result_df
+
+
+# 1000행 이상의 결과값 추출 하는 경우 활용
+def fetchall_athena(database, query):
+    client = boto3.client('athena', region_name='ap-northeast-2')
+    query_id = client.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={
+            'Database': database
+        },
+        ResultConfiguration={
+            'OutputLocation': 's3://data-consulting-private/Unsaved/'
+        }
+    )['QueryExecutionId']
+
+    query_status = None
+    while query_status == 'QUEUED' or query_status == 'RUNNING' or query_status is None:
+        query_status = client.get_query_execution(QueryExecutionId=query_id)['QueryExecution']['Status']['State']
+        if query_status == 'FAILED' or query_status == 'CANCELLED':
+            raise Exception('Athena query with the string "{}" failed or was cancelled'.format(query))
+        time.sleep(10)
+
+    results_paginator = client.get_paginator('get_query_results')
+    results_iter = results_paginator.paginate(
+        QueryExecutionId=query_id,
+        PaginationConfig={
+            'PageSize': 1000
+        }
+    )
+
+    data_list = []
+    for results_page in results_iter:
+        for row in results_page['ResultSet']['Rows']:
+            data_list.append(row['Data'])
+
+    column_names = [x['VarCharValue'] for x in data_list[0]]
+
+    results = []
+    for datum in data_list[1:]:
+        results.append([x['VarCharValue'] if 'VarCharValue' in x.keys() else '' for x in datum])
+
+    df = pd.DataFrame(results, columns=column_names)
+
+    return df
 
